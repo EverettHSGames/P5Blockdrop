c8d479bab69124998f76a417b711dc8e8e4eeeb5
CojanJ <311905@apps.everettsd.org>
2022-06-15 12:19:13 -0700

    a

--- Assets/TitleScreenStart.cs
+++ Assets/TitleScreenStart.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TitleScreenStart : MonoBehaviour
 {
+
+    public string newGameScene;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    // Update is called once per fram
+    // e
     void Update()
     {
         
+    }
+    public void NewGame()
+    {
+        SceneManager.LoadScene(newGameScene);
     }
 }
